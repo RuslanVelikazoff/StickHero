aee764495b3ccc6fa1c4cd8083b07983a8a79014
RuslanVelikazoff <123361452+RuslanVelikazoff@users.noreply.github.com>
2023-10-05 16:49:32 +0300

    Completed

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,85 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Button musicButton;
+    public Sprite musicOnSprite;
+    public Sprite musicOffSprite;
+
+    public Button soundButton;
+    public Sprite soundOnSprite;
+    public Sprite soundOffSprite;
+
+    private void Start()
+    {
+        SetMusicSprite();
+        SetSoundSprite();
+
+        InitializeButton();
+    }
+
+    private void InitializeButton()
+    {
+        if (musicButton != null)
+        {
+            musicButton.onClick.RemoveAllListeners();
+            musicButton.onClick.AddListener(() =>
+            {
+                if (PlayerPrefs.GetFloat("MusicVolume") == 0)
+                {
+                    AudioManager.instance.OnMusic();
+                    SetMusicSprite();
+                }
+                else
+                {
+                    AudioManager.instance.OffMusic();
+                    SetMusicSprite();
+                }
+            });
+        }
+
+        if (soundButton != null)
+        {
+            soundButton.onClick.RemoveAllListeners();
+            soundButton.onClick.AddListener(() =>
+            {
+                if (PlayerPrefs.GetFloat("SoundVolume") == 0)
+                {
+                    AudioManager.instance.OnSound();
+                    SetSoundSprite();
+                }
+                else
+                {
+                    AudioManager.instance.OffSound();
+                    SetSoundSprite();
+                }
+            });
+        }
+    }
+
+    private void SetSoundSprite()
+    {
+        if (PlayerPrefs.GetFloat("SoundVolume") == 0)
+        {
+            soundButton.GetComponent<Image>().sprite = soundOffSprite;
+        }
+        else
+        {
+            soundButton.GetComponent<Image>().sprite = soundOnSprite;
+        }
+    }
+
+    private void SetMusicSprite()
+    {
+        if (PlayerPrefs.GetFloat("MusicVolume") == 0)
+        {
+            musicButton.GetComponent<Image>().sprite = musicOffSprite;
+        }
+        else
+        {
+            musicButton.GetComponent<Image>().sprite = musicOnSprite;
+        }
+    }
+}
